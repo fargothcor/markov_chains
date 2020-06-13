9fecec6fb6a464d2480a0032d96219a7eefefa87
fargothcor <levchenya.roman@gmail.com>
2020-06-13 18:27:05 +0500

    Initial commit

---
+++ aui.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fileencoding=utf-8
+
+import markovify
+
+
+def text_fix():
+    f = open('./in.txt', encoding='utf-8')
+    o = open('./auf.txt', 'w', encoding='utf-8')
+    for i in range(747):
+        line = f.readline()
+        if line != '\n' and line != '***\n':
+            o.write(line)
+
+
+def aue():
+    with open('D:/Projects/CODAME_PROJECTS/markov_chains/auf.txt', encoding='utf-8') as f:
+        text = f.read()
+    text_model = markovify.Text(text, state_size=2)
+    for i in range(20):
+        return text_model.make_short_sentence(380)
+
+
+if __name__ == '__main__':
+    aue()
---
+++ router.py
@@ -0,0 +1,18 @@
+import telebot
+from aui import aue
+
+bot = telebot.TeleBot('1282178960:AAEgAUQThoWDDMq98MoEwxPLsLnrvha6-bU')
+
+
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.send_message(message.chat.id, 'Вечер в Телегу, брат. Научу тебя уму-разуму. '
+                                      'Напиши любое слово или символ - отвечу цитаткой')
+
+
+@bot.message_handler(content_types=['text'])
+def send_text(message):
+    bot.send_message(message.chat.id, aue())
+
+
+bot.polling()
